63eae8c9044b0195da091ee43d7fdb182e94806f
marques <m4rques1s0n@gmail.com>
2025-03-01 16:52:33 +0000

    add

--- data/PermissionsDB.cs
+++ data/PermissionsDB.cs
@@ -10,7 +10,20 @@ namespace AtivoPlus.Data
         public DbSet<Permission> Permissions { get; set; }
         public DbSet<UserPermission> UserPermission { get; set; }
 
+        public async Task<List<Permission>> DoesUserHavePermission(int userId, int[] permissions)
+        {
+            List<Permission> perms = new();
 
+            foreach (int perm in permissions)
+            {
+                var permission = await Permissions.FindAsync(perm);
+                if (permission != null)
+                {
+                    perms.Add(permission);
+                }
+            }
 
+            return perms;
+        }
     }
 }
---
+++ logic/PermissionLogic.cs
@@ -0,0 +1,13 @@
+using AtivoPlus.Models;
+using AtivoPlus.Data;
+using Microsoft.AspNetCore.Http;
+
+namespace AtivoPlus.Logic
+{
+    class PermissionLogic
+    {
+        public void CheckPermission(int[] pemrs)
+        {
+        }
+    }
+}
